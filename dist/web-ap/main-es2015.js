26ef59955e49478d49abd4a3bc7eb1083b128d6e
Jonathas <joninhaevang@gmail.com>
2019-09-03 17:28:59 -0300

    adding dist paste

--- dist/web-ap/main-es2015.js
+++ dist/web-ap/main-es2015.js
@@ -627,7 +627,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_2__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/jonathas/web-ap/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /home/jonathas/Documents/web-ap/src/main.ts */"./src/main.ts");
 
 
 /***/ })
--- dist/web-ap/styles-es2015.js
+++ dist/web-ap/styles-es2015.js
@@ -598,8 +598,8 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/jonathas/web-ap/node_modules/@angular/material/prebuilt-themes/indigo-pink.css */"./node_modules/@angular/material/prebuilt-themes/indigo-pink.css");
-module.exports = __webpack_require__(/*! /home/jonathas/web-ap/src/styles.css */"./src/styles.css");
+__webpack_require__(/*! /home/jonathas/Documents/web-ap/node_modules/@angular/material/prebuilt-themes/indigo-pink.css */"./node_modules/@angular/material/prebuilt-themes/indigo-pink.css");
+module.exports = __webpack_require__(/*! /home/jonathas/Documents/web-ap/src/styles.css */"./src/styles.css");
 
 
 /***/ })
